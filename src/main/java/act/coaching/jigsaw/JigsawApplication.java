8a25c884dc174c3a30fc35ab7e287ad9b16b7646
milo <kajsss@gmail.com>
2017-11-16 18:00:17 +0900

    first commit

---
+++ src/main/java/act/coaching/jigsaw/JigsawApplication.java
@@ -0,0 +1,13 @@
+package act.coaching.jigsaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JigsawApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JigsawApplication.class, args);
+	}
+}
---
+++ src/test/java/act/coaching/peerreview/JigsawApplicationTests.java
@@ -0,0 +1,16 @@
+package act.coaching.peerreview;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JigsawApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
